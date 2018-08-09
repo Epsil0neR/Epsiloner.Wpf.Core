b9fe2d1baa7e66b61b75a541d9dffb8c0e5de33f
Epsil0neR <epsil0neris@gmail.com>
2018-08-09 14:24:00 +0300

    Migrated from .NET Standard 2.0 to .NET Framework 4.7 because of WPF libraries.

---
+++ Epsiloner.Wpf.Core/Epsiloner.Wpf.Core/ViewModels/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Epsiloner.Wpf.ViewModels
+{
+    public interface IViewModel : INotifyPropertyChanged
+    {
+    }
+
+    /// <summary>
+    /// Base view model.
+    /// </summary>
+    public abstract class ViewModel : IViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Sets new value for backing field and raises <see cref="PropertyChanged"/> event for <see cref="propertyName"/> and all depending properties.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="backingField">Backing field</param>
+        /// <param name="newValue">new value</param>
+        /// <param name="propertyName">Property name</param>
+        /// <param name="dependingPropertyNames">Depending properties</param>
+        /// <returns></returns>
+        protected bool Set<T>(ref T backingField, T newValue, [CallerMemberName] string propertyName = null, params string[] dependingPropertyNames)
+        {
+            bool valueChanged = !EqualityComparer<T>.Default.Equals(backingField, newValue);
+            if (valueChanged)
+            {
+                backingField = newValue;
+                RaisePropertyChanged(propertyName);
+
+                if (dependingPropertyNames != null)
+                    foreach (var name in dependingPropertyNames)
+                        RaisePropertyChanged(name);
+            }
+            return valueChanged;
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
